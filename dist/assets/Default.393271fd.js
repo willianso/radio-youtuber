29cb1bfb31f4a3eefc15da655535caba3f5021cf
willian <willian.opiechon@engesoftware.com.br>
2023-02-03 22:29:57 -0300

    test

--- dist/assets/Default.393271fd.js
+++
@@ -1,1 +0,0 @@
-import{r as c,o as e,c as t,w as a,a as r,V as u,b as p}from"./index.b15e167a.js";const l={__name:"View",setup(_){return(o,n)=>{const s=c("router-view");return e(),t(u,null,{default:a(()=>[r(s)]),_:1})}}},m={__name:"Default",setup(_){return(o,n)=>(e(),t(p,null,{default:a(()=>[r(l)]),_:1}))}};export{m as default};
