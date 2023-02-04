244868255212f9ffb3e6d4d9ab694857fb9e2b60
willian <willian.opiechon@engesoftware.com.br>
2023-02-03 22:51:01 -0300

    test

--- src/store/songs.js
+++ src/store/songs.js
@@ -1,10 +1,91 @@
 // Utilities
 import { defineStore } from "pinia";
-import state from "./state";
-import actions from "./actions";
+import Localbase from "localbase";
+
+let db = new Localbase("db");
 
 export const useSongsStore = defineStore("songs", {
   id: "songs",
-  state: () => state,
-  actions,
+  state: () => ({
+    items: [],
+    id: 1,
+  }),
+  actions: {
+    getSongs() {
+      db.collection("songs")
+        .get()
+        .then((songs) => {
+          this.items = songs;
+        });
+    },
+    addSong(newSong) {
+      const id = new Date().getTime();
+
+      db.collection("songs")
+        .add({
+          id,
+          title: newSong.title,
+          value: newSong.value,
+          position: newSong.position,
+          played: false,
+        })
+        .then((response) => {
+          this.items.push({
+            id,
+            title: newSong.title,
+            value: newSong.value,
+            position: newSong.position,
+            played: false,
+          });
+        })
+        .catch((error) => {
+          console.log("There was an error, do something else.");
+        });
+    },
+    removeSong(id) {
+      db.collection("songs")
+        .doc({ id })
+        .delete()
+        .then((response) => {
+          const index = this.items.findIndex((song) => song.id === id);
+          if (index !== -1) this.items.splice(index, 1);
+        })
+        .catch((error) => {
+          console.log("There was an error, do something else.");
+        });
+    },
+    editSong(newSong) {
+      db.collection("songs")
+        .doc({ id: newSong.id })
+        .update({
+          title: newSong.title,
+        })
+        .then(() => {
+          let song = this.items.find((song) => song.id === newSong.id);
+          if (song) song.title = newSong.title;
+        });
+    },
+    playedSong(newSong) {
+      db.collection("songs")
+        .doc({ id: newSong.id })
+        .update({
+          played: !newSong.played,
+        })
+        .then(() => {
+          let song = this.items.find((song) => song.id === newSong.id);
+          if (song) {
+            song.played = !newSong.played;
+          }
+        });
+    },
+    reorderList() {
+      db.collection("songs")
+        .delete()
+        .then(() => {
+          this.items.forEach((item) => {
+            db.collection("songs").add({ ...item });
+          });
+        });
+    },
+  },
 });
